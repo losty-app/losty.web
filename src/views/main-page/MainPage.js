c884e39d77cd941bf4e0bbd7d91a2c85ef166c22
Yonatan Cohen <81573021+yonac1@users.noreply.github.com>
2024-02-07 16:31:12 +0200

    feat(MainPage.js): updated

--- src/views/main-page/MainPage.js
+++ src/views/main-page/MainPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Typography } from "@mui/material";
 import PageContainer from "src/components/container/PageContainer";
 import DashboardCard from "../../components/shared/DashboardCard";
+import LiveRequesters from "./components/LiveRequesters";
 
 const MainPage = () => {
   return (
@@ -11,8 +12,9 @@ const MainPage = () => {
     >
       <DashboardCard title="מסך ראשי">
         <Typography>במסך זה תהיה רשימת החניכים ו האירועים של העמותה</Typography>
-        <SosEvents
       </DashboardCard>
+      <LiveRequesters />
+      <SosEvents />
     </PageContainer>
   );
 };
