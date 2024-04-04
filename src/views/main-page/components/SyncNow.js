9f42a8be5ece596c8f79e1ed07a9ba17f467670d
Yonatan Cohen <81573021+yonac1@users.noreply.github.com>
2024-04-04 19:34:38 +0300

    feat(SyncNow.js): updated

--- src/views/main-page/components/SyncNow.js
+++ src/views/main-page/components/SyncNow.js
@@ -1,21 +1,25 @@
 import { Refresh } from "@mui/icons-material";
 import { Button, Grid, IconButton, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import useRequesters from "src/hooks/useRequesters";
 import useSosEvents from "src/hooks/useSosEvents";
+import { formatDateToIsraelLocale } from "src/utils/utils";
 
 const SyncNow = () => {
   const dispatch = useDispatch();
   const { fetchRequesters } = useRequesters();
   const { fetchSosEvents } = useSosEvents();
   const lastSyncTime = useSelector((state) => state.lastSyncTime);
+  const [displayText, setDisplayText] = useState("לחצ/י לעדכון הכל");
 
-  const handleSyncNow = () => {
+  const handleSyncNow = async () => {
     try {
-      fetchRequesters();
-      fetchSosEvents();
+      setDisplayText("מעדכן...");
+      await fetchRequesters();
+      await fetchSosEvents();
       dispatch({ type: "SET_LAST_SYNC_TIME", payload: new Date() });
+      setDisplayText("עודכן ב-" + formatDateToIsraelLocale(lastSyncTime));
     } catch (e) {
       console.log("Failed to sync");
     }
@@ -23,28 +27,17 @@ const SyncNow = () => {
   return (
     <div
       onClick={handleSyncNow}
-      style={{ position: "absolute", top: 60, right: "50%", zIndex: 99 }}
+      style={{
+        marginBottom: "1rem",
+      }}
     >
-      <Grid
-        container
-        style={{
-          borderWidth: 2,
-          borderRadius: "20%",
-          backgroundColor: "gray",
-          borderColor: "black",
-        }}
-      >
-        <Typography
-          textAlign={"right"}
-          variant="subtitle2"
-          fontWeight={600}
-          style={{ cursor: "pointer" }}
-        >
-          עודכן ב {lastSyncTime}
-          <IconButton size="small">
+      <Grid container>
+        <Button variant="outlined" color="info" style={{ cursor: "pointer" }}>
+          {displayText}
+          <IconButton size="small" color="info">
             <Refresh />
           </IconButton>
-        </Typography>
+        </Button>
       </Grid>
     </div>
   );
